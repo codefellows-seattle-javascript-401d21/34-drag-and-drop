13d6fbba7f43a7f70a21cc7eaffb0d9c8f4e18fe
Roger <roger.neil.davenport@gmail.com>
2018-03-03 18:14:10 -0800

    'text input finished'

--- lab-roger/ui-challenge/text-input/index.js
+++ lab-roger/ui-challenge/text-input/index.js
@@ -6,8 +6,13 @@ class TextInput extends React.Component {
   render () {
     return (
       <div className={this.props.config.className}>
+        {/* <h3>{this.props.config.text}</h3> */}
         <label htmlFor={this.props.config.id}>{this.props.config.text}</label>
-        <input id={this.props.config.id} type='text' pattern='[a-zA-z]{6,12}' required/>
+        <input id={this.props.config.id} 
+          type='text' 
+          pattern='[a-zA-z]{6,12}' 
+          placeholder='placeholder text'
+          required/>
         <label id='input-error'>Please enter a valid username</label>
 
       </div>
