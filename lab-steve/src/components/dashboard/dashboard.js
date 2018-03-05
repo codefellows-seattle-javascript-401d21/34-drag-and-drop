6d9d25077476fc9c6a09a6351a940d2354f94db3
Steven Carpenter <stevegcarpenter@gmail.com>
2018-03-05 01:38:59 -0800

    Modified the page-title to be outside the green box

--- lab-steve/src/components/dashboard/dashboard.js
+++ lab-steve/src/components/dashboard/dashboard.js
@@ -3,27 +3,30 @@ import { connect } from 'react-redux';
 import { categoryCreate } from '../../actions/category-actions';
 import CategoryForm from '../category/category-form/category-form';
 import CategoryItem from '../category/category-item/category-item';
+import './_dashboard.scss';
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <section className='application'>
-        <h1 className='logo'>Expense Tracker</h1>
+      <section className='container'>
+        <h1 className='page-title'>Expense Tracker</h1>
 
-        <CategoryForm
-          buttonText='Create'
-          onComplete={this.props.dashboardCategoryCreate}
-        />
+        <section className='centsmart-section'>
+          <CategoryForm
+            buttonText='Create'
+            onComplete={this.props.dashboardCategoryCreate}
+          />
 
-        {this.props.categories
-          ? this.props.categories.map(cat => (
-            <CategoryItem
-              key={cat._id}
-              category={cat}
-            />
-          ))
-          : undefined
-        }
+          {this.props.categories
+            ? this.props.categories.map(cat => (
+              <CategoryItem
+                key={cat._id}
+                category={cat}
+              />
+            ))
+            : undefined
+          }
+        </section>
       </section>
     );
   }
